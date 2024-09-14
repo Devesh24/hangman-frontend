6c679374831fe89034e1007c1f39c175bc0fb8f6
Devesh Raghuvanshi <devesh12200@gmail.com>
2024-09-14 21:47:47 +0530

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,11 @@
+import { useEffect } from "react";
+
 function Home() {
-    
+    useEffect(() => {
+      window.location.pathname = "/login"
+    },[])
   return (
     <>
-      
     </>
   );
 }
